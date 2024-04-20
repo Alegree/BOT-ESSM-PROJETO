e57d95c4b45c39cd4ed15a3bf013306303d38708
Rodrigo Alegre <alegreedev@gmail.com>
2024-04-20 10:59:35 +0100

    welcome message edit

--- events/welcome.js
+++ events/welcome.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
   const channelId = "1231174104334467164";
   client.on("guildMemberAdd", (member) => {
-    const message = `Bem vindo <@${member.id}> ao servidor discord da ESSM! Visita o nosso site em https://agml.pt/`;
+    const message = `Bem vindo <@${member.id}> ao servidor de discord da ESSM! Visita o nosso site em https://agml.pt/`;
 
     const channel = member.guild.channels.cache.get(channelId);
     channel.send(message);
